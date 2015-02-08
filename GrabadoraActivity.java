0efc4394c61007cc128b302a5e26cdf95852b102
unknown <rikrdo_18_panda@hotmail.com>
2015-02-07 19:51:21 -0600

    ya manda

--- GrabadoraActivity.java
+++ GrabadoraActivity.java
@@ -3,12 +3,24 @@ package org.example.grabadora;
 import android.app.Activity;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.io.File;
 import java.io.IOException;
+
 
 
 public class GrabadoraActivity extends Activity {
@@ -53,5 +65,64 @@ public class GrabadoraActivity extends Activity {
         }
     }
 
-    
+    public void enviar(View view) throws IOException {
+
+        Log.e(LOG_TAG, "Entro a enviar");
+
+        new subir().execute();
+
+
+
+    }
+
+    private class subir extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+
+            String url = "http://192.168.0.4/prueba.php";
+
+            File audio = new File(fichero);
+
+            try {
+
+                HttpPost httpost = new HttpPost(url);
+
+                HttpClient client = new DefaultHttpClient();
+
+                MultipartEntity entity = new MultipartEntity();
+
+                entity.addPart("myAudioFile", new FileBody(audio));
+
+                httpost.setEntity(entity);
+                HttpResponse response = client.execute(httpost);
+                HttpEntity resEntity = response.getEntity();
+
+                if(resEntity != null)
+                {
+                    String responseStr = EntityUtils.toString(resEntity).trim();
+                    Log.e(LOG_TAG,"response" + responseStr);
+                }
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+
+            Log.e(LOG_TAG, "Enviado");
+        }
+
+
+
+    }
+
+
+
+
+
 }
